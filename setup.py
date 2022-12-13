9ea23f41278dc753ac36b2f4a44529ba34eb8f2f
Waseem Sabir <waseemsabir2000@gmail.com>
2022-12-13 14:57:37 +0500

    Initial Stucture

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="firestore-size",
+    version="0.0.1",
+    author="Waseem Sabir",
+    author_email="waseemsabir99@gmail.com",
+    description="A small package to calculate the approximate size of a firestore document",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(include=['firestore-size']),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
